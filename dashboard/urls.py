7e1263fdb1ff9e02f218da3aac023113da64ad8c
unknown <devs44sunuwar@gmail.com>
2021-07-28 13:28:41 +0545

    dashboard crud,home templates

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -21,10 +21,69 @@ urlpatterns = [
              ModelUpdateView.as_view(), name='model-update'),
     path('dashboard/model/<int:pk>/delete/', ModelDeleteView.as_view(), name='model-delete'),
 
+    # type
+    path('dashboard/type/list/', TypeListView.as_view(), name='types'),
+    path('dashboard/type/create/',  TypeCreateView.as_view(), name='type-create'),
+    path('dashboard/type/<int:pk>/update/',
+              TypeUpdateView.as_view(), name='type-update'),
+    path('dashboard/type/<int:pk>/delete/',  TypeDeleteView.as_view(), name='type-delete'),
+
+    # category
+    path('dashboard/category/list/', CategoryListView.as_view(), name='categories'),
+    path('dashboard/category/create/',  CategoryCreateView.as_view(), name='category-create'),
+    path('dashboard/category/<int:pk>/update/',
+              CategoryUpdateView.as_view(), name='category-update'),
+    path('dashboard/category/<int:pk>/delete/',  CategoryDeleteView.as_view(), name='category-delete'),
+
+    # features
+    path('dashboard/features/list/', FeatureListView.as_view(), name='features'),
+    path('dashboard/features/create/',  FeatureCreateView.as_view(), name='feature-create'),
+    path('dashboard/features/<int:pk>/update/',
+              FeatureUpdateView.as_view(), name='feature-update'),
+    path('dashboard/features/<int:pk>/delete/',  FeatureDeleteView.as_view(), name='feature-delete'),
+
     # Cars
     path('dashboard/cars/list/', CarListView.as_view(), name='cars'),
     path('dashboard/cars/create/', CarCreateView.as_view(), name='cars-create'),
     path('dashboard/cars/<int:pk>/update/',
          CarUpdateView.as_view(), name='cars-update'),
     path('dashboard/cars/<int:pk>/delete/', CarDeleteView.as_view(), name='cars-delete'),
+    path('dashboard/cars/image/create/', CarImageCreateView.as_view(), name='cars-image-create'),
+
+    # about
+    path('dashboard/about/list/', AboutListView.as_view(), name='abouts'),
+    path('dashboard/about/create/',  AboutCreateView.as_view(), name='about-create'),
+    path('dashboard/about/<int:pk>/update/',
+              AboutUpdateView.as_view(), name='about-update'),
+    path('dashboard/about/<int:pk>/delete/',  AboutDeleteView.as_view(), name='about-delete'),
+
+    # team
+    path('dashboard/team/list/', TeamListView.as_view(), name='teams'),
+    path('dashboard/team/create/',  TeamCreateView.as_view(), name='team-create'),
+    path('dashboard/team/<int:pk>/update/',
+              TeamUpdateView.as_view(), name='team-update'),
+    path('dashboard/team/<int:pk>/delete/',  TeamtDeleteView.as_view(), name='team-delete'),
+
+    # services
+    path('dashboard/services/list/', ServiceListView.as_view(), name='services'),
+    path('dashboard/services/create/',  ServiceCreateView.as_view(), name='service-create'),
+    path('dashboard/services/<int:pk>/update/',
+              ServiceUpdateView.as_view(), name='service-update'),
+    path('dashboard/services/<int:pk>/delete/',  ServiceDeleteView.as_view(), name='service-delete'),
+
+
+    # services video
+    path('dashboard/services-video/list/', ServiceVideoListView.as_view(), name='services-video'),
+    path('dashboard/services-video/create/',  ServiceVideoCreateView.as_view(), name='service-video-create'),
+    path('dashboard/services-video/<int:pk>/update/',
+              ServiceVideoUpdateView.as_view(), name='service-video-update'),
+    path('dashboard/services-video/<int:pk>/delete/',  ServiceVideoDeleteView.as_view(), name='service-video-delete'),
+
+    # contact
+    path('dashboard/contact/list/', ContactListView.as_view(), name='contacts'),
+    path('dashboard/contact/create/',  ContactCreateView.as_view(), name='contact-create'),
+    path('dashboard/contact/<int:pk>/update/',
+              ContactUpdateView.as_view(), name='contact-update'),
+    path('dashboard/contact/<int:pk>/delete/',  ContactDeleteView.as_view(), name='contact-delete'),
+
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+
+from django.http import JsonResponse
 from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
@@ -41,6 +42,7 @@ class BrandDeleteView(DeleteMixin, DeleteView):
     model = Brand
     success_url = reverse_lazy('dashboard:brands')
 
+
 # models
 class ModelListView(NonDeletedListMixin,ListView):
     template_name = 'dashboard/model/list.html'
@@ -73,6 +75,99 @@ class ModelDeleteView(DeleteMixin,DeleteView):
     success_url = reverse_lazy('dashboard:models')
 
 
+# types
+class TypeListView(NonDeletedListMixin,ListView):
+    template_name = 'dashboard/type/list.html'
+    model = Type
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if "title" in self.request.GET:
+            if self.request.GET.get('title') != '':
+                queryset = queryset.filter(
+                    name__contains=self.request.GET.get("title"))
+        return queryset
+
+
+class TypeCreateView(CreateView):
+    template_name = 'dashboard/type/form.html'
+    form_class = TypeForm
+    success_url = reverse_lazy('dashboard:types')
+
+
+class TypeUpdateView(UpdateView):
+    template_name = 'dashboard/type/form.html'
+    model = Type
+    form_class = TypeForm
+    success_url = reverse_lazy('dashboard:types')
+
+
+class TypeDeleteView(DeleteMixin,DeleteView):
+    model = Type
+    success_url = reverse_lazy('dashboard:types')
+
+# category
+class CategoryListView(NonDeletedListMixin,ListView):
+    template_name = 'dashboard/category/list.html'
+    model = Category
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if "title" in self.request.GET:
+            if self.request.GET.get('title') != '':
+                queryset = queryset.filter(
+                    name__contains=self.request.GET.get("title"))
+        return queryset
+
+
+class CategoryCreateView(CreateView):
+    template_name = 'dashboard/category/form.html'
+    form_class = CategoryForm
+    success_url = reverse_lazy('dashboard:categories')
+
+
+class CategoryUpdateView(UpdateView):
+    template_name = 'dashboard/category/form.html'
+    model = Category
+    form_class = CategoryForm
+    success_url = reverse_lazy('dashboard:categories')
+
+
+class CategoryDeleteView(DeleteMixin,DeleteView):
+    model = Category
+    success_url = reverse_lazy('dashboard:categories')
+
+# features
+class FeatureListView(NonDeletedListMixin,ListView):
+    template_name = 'dashboard/features/list.html'
+    model = Features
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if "title" in self.request.GET:
+            if self.request.GET.get('title') != '':
+                queryset = queryset.filter(
+                    name__contains=self.request.GET.get("title"))
+        return queryset
+
+
+class FeatureCreateView(CreateView):
+    template_name = 'dashboard/features/form.html'
+    form_class = FeatureForm
+    success_url = reverse_lazy('dashboard:features')
+
+
+class FeatureUpdateView(UpdateView):
+    template_name = 'dashboard/features/form.html'
+    model = Features
+    form_class = FeatureForm
+    success_url = reverse_lazy('dashboard:features')
+
+
+class FeatureDeleteView(DeleteMixin,DeleteView):
+    model = Features
+    success_url = reverse_lazy('dashboard:features')
+
 # cars
 class CarListView(NonDeletedListMixin,ListView):
     template_name = 'dashboard/cars/list.html'
@@ -86,6 +181,28 @@ class CarListView(NonDeletedListMixin,ListView):
                     name__contains=self.request.GET.get("name"))
         return queryset
 
+class CarImageCreateView(CreateView):
+    model = CarImage
+    form_class = CarImageForm
+    template_name = "dashboard/car/imageform.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.request.is_ajax():
+            return super().dispatch(request, *args, **kwargs)
+        return JsonResponse({"error": "Cannot access this page"}, status=404)
+
+    def form_valid(self, form):
+        instance = form.save()
+        return JsonResponse(
+            {
+                "status": "ok",
+                "pk": instance.pk,
+                "url": instance.image.url,
+            }
+        )
+
+    def form_invalid(self, form):
+        return JsonResponse({"errors": form.errors}, status=400)
 
 class CarCreateView(CreateView):
     template_name = 'dashboard/cars/form.html'
@@ -103,3 +220,119 @@ class CarUpdateView(UpdateView):
 class CarDeleteView(DeleteMixin, DeleteView):
     model = Car
     success_url = reverse_lazy('dashboard:cars')
+
+# abouts
+class AboutListView(NonDeletedListMixin,ListView):
+    template_name = 'dashboard/about/list.html'
+    model = About
+
+
+class AboutCreateView(CreateView):
+    template_name = 'dashboard/about/form.html'
+    form_class = AboutForm
+    success_url = reverse_lazy('dashboard:abouts')
+
+
+class AboutUpdateView(UpdateView):
+    template_name = 'dashboard/about/form.html'
+    model = About
+    form_class = AboutForm
+    success_url = reverse_lazy('dashboard:abouts')
+
+
+class AboutDeleteView(DeleteMixin,DeleteView):
+    model = About
+    success_url = reverse_lazy('dashboard:abouts')
+
+# teams
+class TeamListView(NonDeletedListMixin,ListView):
+    template_name = 'dashboard/team/list.html'
+    model = Team
+
+
+class TeamCreateView(CreateView):
+    template_name = 'dashboard/team/form.html'
+    form_class = TeamForm
+    success_url = reverse_lazy('dashboard:teams')
+
+
+class TeamUpdateView(UpdateView):
+    template_name = 'dashboard/team/form.html'
+    model = Team
+    form_class = TeamForm
+    success_url = reverse_lazy('dashboard:teams')
+
+
+class TeamtDeleteView(DeleteMixin,DeleteView):
+    model = Team
+    success_url = reverse_lazy('dashboard:teams')
+
+# services
+class ServiceListView(NonDeletedListMixin,ListView):
+    template_name = 'dashboard/services/list.html'
+    model = Services
+
+
+class ServiceCreateView(CreateView):
+    template_name = 'dashboard/services/form.html'
+    form_class = ServicesForm
+    success_url = reverse_lazy('dashboard:services')
+
+
+class ServiceUpdateView(UpdateView):
+    template_name = 'dashboard/services/form.html'
+    model = Services
+    form_class = ServicesForm
+    success_url = reverse_lazy('dashboard:services')
+
+
+class ServiceDeleteView(DeleteMixin,DeleteView):
+    model = Services
+    success_url = reverse_lazy('dashboard:services')
+
+
+# services-video
+class ServiceVideoListView(NonDeletedListMixin,ListView):
+    template_name = 'dashboard/services-video/list.html'
+    model = ServicesVideo
+
+
+class ServiceVideoCreateView(CreateView):
+    template_name = 'dashboard/services-video/form.html'
+    form_class = ServicesVideoForm
+    success_url = reverse_lazy('dashboard:services-video')
+
+
+class ServiceVideoUpdateView(UpdateView):
+    template_name = 'dashboard/services-video/form.html'
+    model = ServicesVideo
+    form_class = ServicesVideoForm
+    success_url = reverse_lazy('dashboard:services-video')
+
+
+class ServiceVideoDeleteView(DeleteMixin,DeleteView):
+    model = ServicesVideo
+    success_url = reverse_lazy('dashboard:services-video')
+
+# contact
+class ContactListView(NonDeletedListMixin,ListView):
+    template_name = 'dashboard/contact/list.html'
+    model = Contact
+
+
+class ContactCreateView(CreateView):
+    template_name = 'dashboard/contact/form.html'
+    form_class = ContactForm
+    success_url = reverse_lazy('dashboard:contacts')
+
+
+class ContactUpdateView(UpdateView):
+    template_name = 'dashboard/contact/form.html'
+    model = Contact
+    form_class = ContactForm
+    success_url = reverse_lazy('dashboard:contacts')
+
+
+class ContactDeleteView(DeleteMixin,DeleteView):
+    model = Contact
+    success_url = reverse_lazy('dashboard:contacts')
